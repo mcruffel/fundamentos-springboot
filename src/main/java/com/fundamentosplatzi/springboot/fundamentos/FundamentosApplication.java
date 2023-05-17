472df63d2d97ac557d7ab18d22f8b39623be2069
mcruffel <hector.sanchezt@pragma.com.co>
2023-05-17 10:31:38 -0500

    cargando

--- src/main/java/com/fundamentosplatzi/springboot/fundamentos/FundamentosApplication.java
+++ src/main/java/com/fundamentosplatzi/springboot/fundamentos/FundamentosApplication.java
@@ -8,6 +8,7 @@ public class FundamentosApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FundamentosApplication.class, args);
+		System.out.println("dafdfdasfdsaf");
 	}
 
 }
